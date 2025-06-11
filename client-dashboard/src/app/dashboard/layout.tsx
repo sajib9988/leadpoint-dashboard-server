8d58dd1039c667dcd48a42859fceaf7241c727da
sajib9988 <sajib.biswas9988@gmail.com>
2025-06-11 11:12:57 +0600

    uu

--- client-dashboard/src/app/dashboard/layout.tsx
+++ client-dashboard/src/app/dashboard/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
+import { SidebarInset, SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import App from "next/app";
-
+import { AppSidebar } from "@/components/app-sidebar";
 import { Toaster } from "sonner";
 
 const geistSans = Geist({
@@ -32,14 +32,18 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
 
+ <SidebarProvider>
+      <AppSidebar />
+      <SidebarInset>
         <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
           <div className="flex items-center gap-2 px-4">
-
+            <SidebarTrigger className="-ml-1" />
           </div>
         </header>
               <Toaster richColors position="top-center" />
         <div className="p-4 pt-0 min-h-screen">{children}</div>
-
+      </SidebarInset>
+    </SidebarProvider>
        
 
 
